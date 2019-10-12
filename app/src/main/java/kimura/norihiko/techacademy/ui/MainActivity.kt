1a4c7479f7323fb4523ae3495def0eee7203847a
nkimura-git <n.kimura.0623@gmail.com>
2019-10-13 08:00:57 +0900

    Initial commit

---
+++ app/src/main/java/kimura/norihiko/techacademy/ui/MainActivity.kt
@@ -0,0 +1,21 @@
+package kimura.norihiko.techacademy.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() , View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button1.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        Log.d("UI_PARTS", "ボタンをタップしました")
+    }
+}
